a71b3fcd3f3472a9d94a564d7eebe3251122d0ec
siddharthg <siddharthg.official@gmail.com>
2017-08-30 13:58:57 +0530

    [ANIM][SUPPORT] Add Animation Support

---
+++ app/src/main/java/com/a3isummit/animations/AnimHelper.java
@@ -0,0 +1,159 @@
+package com.a3isummit.animations;
+
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.content.Context;
+import android.view.View;
+
+import com.a3isummit.debug.Dbg;
+
+/**
+ * Helper class. Exposes APIs to animate views and handles different types of animations
+ */
+
+public class AnimHelper
+{
+    // ----------------------- Constants ----------------------- //
+    private static final String TAG = "ANIM_HELPER";
+
+    private static final int ANIM_DURATION_MS = 500;
+
+    // ----------------------- Classes ---------------------------//
+    // ----------------------- Interfaces ----------------------- //
+    // ----------------------- Globals ----------------------- //
+    private Context context = null;
+
+    // ----------------------- Constructor ----------------------- //
+    public AnimHelper(Context context)
+    {
+        this.context = context;
+    }
+
+    // ----------------------- Overrides ----------------------- //
+    // ----------------------- Public APIs ----------------------- //
+    public void Animate(final AnimObject.Base anim)
+    {
+        // Get Animator
+        ObjectAnimator animator = GetAnimator(anim);
+        if (animator == null)
+        {
+            Dbg.error(TAG, "Animator not available");
+            return;
+        }
+
+        // Set common properties on animator
+        animator.setDuration(ANIM_DURATION_MS);
+        animator.setInterpolator(anim.interpolator);
+        animator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation)
+            {
+                switch (anim.type)
+                {
+                    case AnimObject.TYPE_FADE_IN:
+                    case AnimObject.TYPE_ROTATE_WITH_FADE_IN:
+                    {
+                        anim.view.setVisibility(View.VISIBLE);
+                        break;
+                    }
+                }
+
+                if (anim.listener != null)
+                {
+                    anim.listener.onAnimationStart(animation);
+                }
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation)
+            {
+                switch (anim.type)
+                {
+                    case AnimObject.TYPE_FADE_OUT:
+                    case AnimObject.TYPE_ROTATE_WITH_FADE_OUT:
+                    {
+                        anim.view.setVisibility(View.GONE);
+                        anim.view.setAlpha(1.0f);
+                        break;
+                    }
+                }
+
+                if (anim.listener != null)
+                {
+                    anim.listener.onAnimationEnd(animation);
+                }
+
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation)
+            {
+                if (anim.listener != null)
+                {
+                    anim.listener.onAnimationCancel(animation);
+                }
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation)
+            {
+                if (anim.listener != null)
+                {
+                    anim.listener.onAnimationRepeat(animation);
+                }
+            }
+        });
+
+        // Start Animation
+        animator.start();
+    }
+
+    // ----------------------- Private APIs ----------------------- //
+    private ObjectAnimator GetAnimator(AnimObject.Base anim)
+    {
+        ObjectAnimator animator = null;
+
+        switch (anim.type)
+        {
+            case AnimObject.TYPE_FADE_IN:
+            {
+                animator = ObjectAnimator.ofFloat(anim.view, "alpha", 0.0f, 1.0f);
+                break;
+            }
+            case AnimObject.TYPE_FADE_OUT:
+            {
+                animator = ObjectAnimator.ofFloat(anim.view, "alpha", 1.0f, 0.0f);
+                break;
+            }
+            case AnimObject.TYPE_SLIDE:
+            {
+                AnimObject.Slide slideAnim = (AnimObject.Slide) anim;
+                if (slideAnim.slideAxis == AnimObject.Slide.SLIDE_AXIS_X)
+                {
+                    animator = ObjectAnimator.ofFloat(anim.view, "translationX", slideAnim.startF, slideAnim.endF);
+                }
+                else
+                {
+                    animator = ObjectAnimator.ofFloat(anim.view, "translationY", slideAnim.startF, slideAnim.endF);
+                }
+                break;
+            }
+            case AnimObject.TYPE_ROTATE_WITH_FADE_OUT:
+            {
+                PropertyValuesHolder rotate = PropertyValuesHolder.ofFloat("rotation", 0.0f, 135.0f);
+                PropertyValuesHolder fade = PropertyValuesHolder.ofFloat("alpha", 1.0f, 0.0f);
+                animator = ObjectAnimator.ofPropertyValuesHolder(anim.view, fade, rotate);
+                break;
+            }
+            case AnimObject.TYPE_ROTATE_WITH_FADE_IN:
+            {
+                PropertyValuesHolder rotate = PropertyValuesHolder.ofFloat("rotation", -135.0f, 0.0f);
+                PropertyValuesHolder fade = PropertyValuesHolder.ofFloat("alpha", 0.0f, 1.0f);
+                animator = ObjectAnimator.ofPropertyValuesHolder(anim.view, fade, rotate);
+                break;
+            }
+        }
+        return animator;
+    }
+}
---
+++ app/src/main/java/com/a3isummit/animations/PowerInterpolator.java
@@ -0,0 +1,39 @@
+package com.a3isummit.animations;
+
+/**
+ * Created by Siddharth on 30-08-2017.
+ */
+
+public class PowerInterpolator extends BaseInterpolator
+{
+    // ----------------------- Constants ----------------------- //
+    private static final String TAG = "POWER_INTERPOLATOR";
+
+    // ----------------------- Classes ---------------------------//
+    // ----------------------- Interfaces ----------------------- //
+    // ----------------------- Globals ----------------------- //
+    public int power = 0;
+
+    // ----------------------- Constructor ----------------------- //
+    public PowerInterpolator(boolean bIn, int power)
+    {
+        super(bIn);
+        this.power = power;
+    }
+
+    // ----------------------- Overrides ----------------------- //
+    @Override
+    public float GetIn(float t)
+    {
+        return In(t, power);
+    }
+
+    @Override
+    public float GetOut(float t)
+    {
+        return Out(t, power);
+    }
+
+    // ----------------------- Public APIs ----------------------- //
+    // ----------------------- Private APIs ----------------------- //
+}
