ef099cb60b1b164fd119c7e365d8fb6b76562067
jagan241296 <jagan241296@gmail.com>
2017-08-30 15:39:56 +0530

    Added count down functionality....

---
+++ app/src/main/java/com/a3isummit/threeisummit/CountDownActivity.java
@@ -0,0 +1,80 @@
+package com.a3isummit.threeisummit;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class CountDownActivity extends AppCompatActivity {
+
+
+    private TextView txtDay, txtHour, txtMinute, txtSecond;
+    private TextView tvEventStart;
+    private Handler handler;
+    private Runnable runnable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtDay = (TextView) findViewById(R.id.txtDay);
+        txtHour = (TextView) findViewById(R.id.txtHour);
+        txtMinute = (TextView) findViewById(R.id.txtMinute);
+        txtSecond = (TextView) findViewById(R.id.txtSecond);
+        tvEventStart = (TextView) findViewById(R.id.tveventStart);
+
+        countDownStart();
+    }
+
+    public void countDownStart() {
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                handler.postDelayed(this, 1000);
+                try {
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                    // Please here set your event date//YYYY-MM-DD
+                    Date futureDate = dateFormat.parse("2017-09-25");
+                    Date currentDate = new Date();
+                    if (!currentDate.after(futureDate)) {
+                        long diff = futureDate.getTime()
+                                - currentDate.getTime();
+                        long days = diff / (24 * 60 * 60 * 1000);
+                        diff -= days * (24 * 60 * 60 * 1000);
+                        long hours = diff / (60 * 60 * 1000);
+                        diff -= hours * (60 * 60 * 1000);
+                        long minutes = diff / (60 * 1000);
+                        diff -= minutes * (60 * 1000);
+                        long seconds = diff / 1000;
+                        txtDay.setText("" + String.format("%02d", days));
+                        txtHour.setText("" + String.format("%02d", hours));
+                        txtMinute.setText(""+ String.format("%02d", minutes));
+                        txtSecond.setText(""+ String.format("%02d", seconds));
+                    } else {
+                        tvEventStart.setVisibility(View.VISIBLE);
+                        tvEventStart.setText("The Event started");
+                        textViewGone();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        handler.postDelayed(runnable, 1 * 1000);
+    }
+
+    public void textViewGone() {
+        findViewById(R.id.LinearLayout1).setVisibility(View.GONE);
+        findViewById(R.id.LinearLayout2).setVisibility(View.GONE);
+        findViewById(R.id.LinearLayout3).setVisibility(View.GONE);
+        findViewById(R.id.LinearLayout4).setVisibility(View.GONE);
+        findViewById(R.id.textViewheader1).setVisibility(View.GONE);
+        findViewById(R.id.textViewheader2).setVisibility(View.GONE);
+    }
+}
