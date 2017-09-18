e63db292594b09429d49e0194cc8d26fe7615efc
jagan241296 <jagan241296@gmail.com>
2017-09-18 10:47:21 +0530

    done few changes with fcm

---
+++ app/src/main/java/com/a3isummit/threeisummit/MyFirebaseInstanceIDService.java
@@ -0,0 +1,25 @@
+package com.a3isummit.threeisummit;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by Sai_Kameswari on 18-09-2017.
+ */
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("Refreshed Token: ", "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        //sendRegistrationToServer(refreshedToken);
+    }
+}
